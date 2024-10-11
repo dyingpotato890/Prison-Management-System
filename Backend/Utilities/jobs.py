1fee92bf2e5efa3fd3f730434b4ac6e713fde315
dyingpotato890 <niranjayajayan2004@gmail.com>
2024-10-12 00:00:40 +0530

    Added Job Functions

--- Backend/Utilities/jobs.py
+++ Backend/Utilities/jobs.py
@@ -1,6 +1,38 @@
+from Utilities.connector import Connector
 
 class Job:
-    #Job related funtions
+    def __init__(self, db_connection) -> None:
+        self.db = Connector()
 
-    def __init__(self) -> None:
-        pass
+    def addJob(self, job_desc: str, work_start: str, work_end: str) -> None:
+        self.db.cursor.execute(
+            "INSERT INTO JOBS (JOB_DESC, WORK_START, WORK_END) VALUES (%s, %s, %s)",
+            (job_desc, work_start, work_end))
+        self.db.conn.commit()
+
+    def updateJob(self, job_id: int, job_desc: str, work_start: str, work_end: str) -> None:
+        self.db.cursor.execute(
+            "UPDATE JOBS SET JOB_DESC = %s, WORK_START = %s, WORK_END = %s WHERE JOBID = %s",
+            (job_desc, work_start, work_end, job_id))
+        self.db.conn.commit()
+
+    def deleteJob(self, job_id: int) -> None:
+        self.db.cursor.execute("DELETE FROM JOBS WHERE JOBID = %s", (job_id,))
+        self.db.conn.commit()
+
+    def assignJob(self, prisoner_id: int, job_id: int, hours_worked: float) -> None:
+        self.db.cursor.execute(
+            "INSERT INTO WORK (PRISONER_ID, JOBID, HOURS_WORKED) VALUES (%s, %s, %s)",
+            (prisoner_id, job_id, hours_worked))
+        self.db.conn.commit()
+
+    def updateHoursWorked(self, prisoner_id: int, job_id: int, hours_worked: float) -> None:
+        self.db.cursor.execute(
+            "UPDATE WORK SET HOURS_WORKED = %s WHERE PRISONER_ID = %s AND JOBID = %s",
+            (hours_worked, prisoner_id, job_id))
+        self.db.conn.commit()
+
+    def totalHours(self, prisoner_id: int) -> float:
+        self.db.cursor.execute("SELECT HOURS_WORKED FROM WORK WHERE PRISONER_ID = %s",(prisoner_id,))
+        total_hours = self.db.cursor.fetchone()[0]
+        return total_hours if total_hours else 0.0
